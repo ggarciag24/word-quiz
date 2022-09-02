bf64d605a5a4d58070fb9c683104a1a55d89920a
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-02 13:08:45 -0400

    <Answer /> has been styled

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Answer(){
     return (
-        <div>
-            <input type='text' id='answer' name='answer' placeholder='Answer goes here...'/>
-            <button type='submit'>Guess</button>
+        <div className='is-flex is-flex-direction-column is-justify-content-center mt-6'>
+            <input className='answer-input input is-info' type='text' id='answer' name='answer' placeholder='Answer goes here...'/>
+            <button className='guess-btn has-background-info has-text-white mt-6' type='submit'>Guess</button>
         </div>
     )
 }
