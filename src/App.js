3f72615689335115b5ff794b467ba7bbfea67cee
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-01 23:53:55 -0400

    <GeneratedDefinition /> was created, imported into App.js and styled(slightly)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './index.css';
 import Header from './components/Header'
 import GenerateButton from './components/GenerateButton';
+import GeneratedDefinition from './components/GeneratedDefinition';
+
 
 function App() {
   return (
     <div className="App">
     <Header />
     <GenerateButton />
+    <GeneratedDefinition /> 
     </div>
   );
 }
---
+++ src/components/GeneratedDefinition.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function GeneratedDefinition(){
+    return (
+        <div className='def is-flex is-justify-content-center mt-6'>
+            <p> this is where the fetched definiton will appear</p>
+        </div>
+    )
+}
+
+export default GeneratedDefinition;
