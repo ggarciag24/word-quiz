0084c2ecad942bf3ecfe17e3c19c497e6e20eed5
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-01 22:52:18 -0400

    <header /> has been styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header(){
     return (
-        <div>
+        <div className='is-size-1 is-flex is-justify-content-center has-background-info has-text-white pt-3 pb-3'>
             Word Quiz
         </div>
     )
