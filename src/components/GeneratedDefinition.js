12cb0628ea3b24c5a8214e701b4c08ee23967302
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-02 16:03:25 -0400

    in <GeneratedDefinition /> instructions are conditionally rendered if there is no definition being displayed

--- src/components/GeneratedDefinition.js
+++ src/components/GeneratedDefinition.js
@@ -5,6 +5,7 @@ function GeneratedDefinition(props){
     return (
         <div className='def is-flex is-justify-content-center mt-6'>
             <p>{props.state.definition}</p>
+            { props.state.definition  === '' && <p> CLICK THE BUTTON ABOVE!</p>}
         </div>
     )
 }
